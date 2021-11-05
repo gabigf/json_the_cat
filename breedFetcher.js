c29b2966aa3d62393399a8d2f3858a2a944d9d5e
Gabriela Guimaraes <gabriela.g.ferreira91@gmail.com>
2021-11-05 01:07:12 +0000

    Added mocha and chai + tests

--- breedFetcher.js
+++ breedFetcher.js
@@ -3,7 +3,7 @@ const request = require('request');
 const fetchBreedDescription = (breedName, callback) => {
 	request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (err, res, body) => {
 		if (err) {
-			callback(err);
+			callback(err, null);
 		}
 		if (res.statusCode !== 200) {
 			callback(`Connection not found, check status: ${res.statusCode}`);
@@ -11,7 +11,8 @@ const fetchBreedDescription = (breedName, callback) => {
 		const data = JSON.parse(body);
 
 		if (data.length === 0) {
-			callback('We could not find that cat breed!');
+			callback('We could not find that cat breed!', null);
+			return;
 		}
 		callback(null, data[0].description);
 		
